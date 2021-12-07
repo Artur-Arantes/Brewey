730833cd79be747c32da47b7dc3370291ddf164a
Artur Arantes T <artur.a.tiraboschi@gmail.com>
2021-12-07 10:06:50 -0300

    initial commit

---
+++ src/main/java/artur/springframework/brewey/BreweyApplication.java
@@ -0,0 +1,13 @@
+package artur.springframework.brewey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreweyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreweyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/artur/springframework/brewey/BreweyApplicationTests.java
@@ -0,0 +1,13 @@
+package artur.springframework.brewey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreweyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
